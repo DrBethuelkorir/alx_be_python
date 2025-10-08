82ed1f7ddf7f2fd58fac0ab9893449988ea52f42
Dr.sheriff <kiptoobethuel44@gmail.com>
2025-10-08 13:17:16 +0300

    output corrected

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author):  # Fixed: removed extra comma
+    def __init__(self, title, author):
         self.title = title
         self.author = author
     
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
     
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author} Page Count : {self.page_count} "
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -37,4 +37,4 @@ class Library:
     
     def list_books(self):
         for book in self.books:
-            print(book)  # Now this will show readable book info!
+            print(book)
